d325829a8ed0ebea3ba85fd76603b13f4ca1e848
OiJs <oijs9663@gmail.com>
2026-05-13 23:46:07 +0900

    feat: ddl

---
+++ src/main/java/com/nhnacademy/associationAPI/MinidoorayTeam8AssociationApiApplication.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.associationAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinidoorayTeam8AssociationApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MinidoorayTeam8AssociationApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nhnacademy/associationAPI/MinidoorayTeam8AssociationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.associationAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinidoorayTeam8AssociationApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
